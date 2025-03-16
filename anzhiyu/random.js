378d9449368015c307d9724c0a3d7af5843ab114
zd7852 <zhoud.sd@gmail.com>
2025-03-16 22:29:14 +0800

    Site updated: 2025-03-16 22:29:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/15/hello-world/","2025/03/15/测试文章/"];function toRandomPost(){
+var posts=["2025/03/15/测试文章/","2025/03/15/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
