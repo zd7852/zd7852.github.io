5c483a34ed9e923370600ef4709d8629c82db820
zd7852 <zhoud.sd@gmail.com>
2025-03-21 19:53:49 +0800

    Site updated: 2025-03-21 19:53:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/15/hello-world/","2025/03/15/测试文章/","2025/03/18/再次测试/","2025/03/17/测试类文章/","2025/03/21/说说易经/"];function toRandomPost(){
+var posts=["2025/03/18/再次测试/","2025/03/17/测试类文章/","2025/03/15/测试文章/","2025/03/21/说说易经/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
