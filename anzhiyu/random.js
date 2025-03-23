c258ecef151aa061ab8da682caf65b2c769636e8
zd7852 <zhoud.sd@gmail.com>
2025-03-23 10:03:04 +0800

    Site updated: 2025-03-23 10:03:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/21/见性与幻觉的辨析/","2025/03/21/jar包导出到Linux里/"];function toRandomPost(){
+var posts=["2025/03/21/jar包导出到Linux里/","2025/03/21/见性与幻觉的辨析/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
