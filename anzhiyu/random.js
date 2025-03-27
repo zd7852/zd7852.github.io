91a54047043510417e4c017c779d8991039136d2
zd7852 <zhoud.sd@gmail.com>
2025-03-27 22:55:44 +0800

    Site updated: 2025-03-27 22:55:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/21/jar包导出到Linux里/","2025/03/27/mysql在centos7中rpm安装问题和方法/","2025/03/27/数据库语言分类及简单介绍/","2025/03/21/见性与幻觉的辨析/"];function toRandomPost(){
+var posts=["2025/03/21/jar包导出到Linux里/","2025/03/27/mysql在centos7中rpm安装问题和方法/","2025/03/21/见性与幻觉的辨析/","2025/03/27/数据库语言分类及简单介绍/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
