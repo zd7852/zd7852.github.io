4c223281840a023c174cf29b06cc8cf933124d95
zd7852 <zhoud.sd@gmail.com>
2025-04-07 21:07:33 +0800

    Site updated: 2025-04-07 21:07:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/21/jar包导出到Linux里/","2025/03/27/mysql在centos7中rpm安装问题和方法/","2025/03/29/就问你苦不苦/","2025/03/27/数据库语言分类及简单介绍/","2025/03/21/见性与幻觉的辨析/","2025/04/01/阿里云一个月免费主机MySQL/"];function toRandomPost(){
+var posts=["2025/03/29/就问你苦不苦/","2025/03/27/mysql在centos7中rpm安装问题和方法/","2025/03/21/jar包导出到Linux里/","2025/03/27/数据库语言分类及简单介绍/","2025/03/21/见性与幻觉的辨析/","2025/04/01/阿里云一个月免费主机MySQL/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
